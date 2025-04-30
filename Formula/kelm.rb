662b7512988eada2ae536e923df466de0da334e2
viveksair <viveksair@ibm.com>
2025-04-30 01:46:18 -0400

    refactor

--- Formula/kelm.rb
+++ Formula/kelm.rb
@@ -1,6 +1,6 @@
 class Kelm < Formula
     desc "Helm-aware kubectl plugin to filter by release"
-    homepage "https://github.com/viveksair/kelm"
+    homepage "git@github.com:viveksair/kelm.git"
     url "https://github.com/viveksair/kelm/releases/download/v0.1.0/kubectl-kelm"
     version "0.1.0"
     sha256 "7d73672bc1d3197c63a5a2d894b7ab3a8b0c12f9fc53a2be3de95174328ae979"
